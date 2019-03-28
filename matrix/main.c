073c765d7960bd08f0b7f07eb7884336e1234e9d
schweigert <marlon.henry@magrathealabs.com>
2019-03-28 16:14:42 -0300

    Calc only sums

--- matrix/main.c
+++ matrix/main.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <sys/time.h>
+#include <locale.h>
 #include "matrix.h"
+
+matrix_t* matrix_one;
+matrix_t* matrix_two;
 
 double wtime() {
    struct timeval t;
@@ -10,20 +14,8 @@ double wtime() {
    return t.tv_sec + t.tv_usec / 1000000.0;
 }
 
-void doSomething(int nrows, int ncols) {
-   matrix_t* matrix_one = matrix_create(nrows, ncols);
-   matrix_randfill(matrix_one);
-
-      matrix_t* matrix_two = matrix_create(nrows, ncols);
-   matrix_randfill(matrix_one);
-
+void doSomething() {
    matrix_t* matrix_r = matrix_sum(matrix_one, matrix_two);
-
-   // matrix_print(matrix_two);
-
-   matrix_destroy(matrix_one);
-   matrix_destroy(matrix_two);
-   matrix_destroy(matrix_r);
 }
 
 int main(int argc, char **argv) {
@@ -38,13 +30,21 @@ int main(int argc, char **argv) {
    nrows = atoi(argv[1]); 
    ncols = atoi(argv[2]); 
 
-   start_time = wtime();
-   
-   doSomething(nrows, ncols);
+   matrix_one = matrix_create(nrows, ncols);
+   matrix_two = matrix_create(nrows, ncols);
 
+   matrix_randfill(matrix_one);
+   matrix_randfill(matrix_two);
+
+   start_time = wtime();
+   doSomething();
    end_time = wtime();
 
-   printf("%d %d %f\n", nrows, ncols, end_time - start_time);
+   matrix_destroy(matrix_one);
+   matrix_destroy(matrix_two);
+
+   setlocale(LC_NUMERIC, "");
+   printf("%'.8f\n", nrows, ncols, end_time - start_time);
    fflush(stdout);
 
    return EXIT_SUCCESS;
